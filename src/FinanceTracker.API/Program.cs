a9ec33996271c93c295a17ba6b9f285eb8383d4c
Tudor Rapanu <tudor.rapanu@gmail.com>
2026-03-27 23:34:08 +0200

    Https Redirection is now done only for development

--- src/FinanceTracker.API/Program.cs
+++ src/FinanceTracker.API/Program.cs
@@ -63,7 +63,11 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
