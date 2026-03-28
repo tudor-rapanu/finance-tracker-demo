a3f2045b2d52a7476d6284cd4c4f88d5067b8e8e
Tudor Rapanu <tudor.rapanu@gmail.com>
2026-03-28 02:00:39 +0200

    Hardcoded API base URL

--- src/FinanceTracker.Web/Program.cs
+++ src/FinanceTracker.Web/Program.cs
@@ -5,8 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-var apiBaseUrl = builder.Configuration["ApiBaseUrl"]
-    ?? "https://finance-tracker-web-app-h7gbepf2dccvfccx.westeurope-01.azurewebsites.net"; // fallback
+var apiBaseUrl = "https://finance-tracker-web-app-h7gbepf2dccvfccx.westeurope-01.azurewebsites.net"; // fallback
 
 builder.Services.AddScoped(sp => new HttpClient
 {
