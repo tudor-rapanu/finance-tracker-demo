7657092804a0c3bc3015fc001565effc909f258b
Tudor Rapanu <tudor.rapanu@gmail.com>
2026-04-01 13:49:09 +0300

    Updated server URL

--- src/FinanceTracker.Web/Program.cs
+++ src/FinanceTracker.Web/Program.cs
@@ -5,8 +5,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-//var apiBaseUrl = "https://finance-tracker-web-app-h7gbepf2dccvfccx.westeurope-01.azurewebsites.net"; // fallback
-var apiBaseUrl = "https://localhost:7100";
+var apiBaseUrl = "https://finance-tracker-web-app-h7gbepf2dccvfccx.westeurope-01.azurewebsites.net"; // fallback
+//var apiBaseUrl = "https://localhost:7100";
 
 builder.Services.AddScoped(sp => new HttpClient
 {
